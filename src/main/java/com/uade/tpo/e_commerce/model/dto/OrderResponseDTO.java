57f4127da0828ae954a5156ef24b7abaf78ab8a0
Nahuel Berensztein <nberensztein13@gmail.com>
2026-04-21 20:38:45 -0300

    DTOs de pedidos

---
+++ src/main/java/com/uade/tpo/e_commerce/model/dto/OrderResponseDTO.java
@@ -0,0 +1,25 @@
+package com.uade.tpo.e_commerce.model.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class OrderResponseDTO {
+    
+    private Long id;
+
+    private LocalDate date;
+
+    private Long userId;
+
+	private List<Long> products;
+
+    private Double total;
+
+}
