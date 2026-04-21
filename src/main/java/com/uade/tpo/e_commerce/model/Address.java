c0f0bc42ca403d60f225a1009b7a604e6966ef86
Nahuel Berensztein <nberensztein13@gmail.com>
2026-04-21 20:34:03 -0300

    entidad de dirección usada en usuario

---
+++ src/main/java/com/uade/tpo/e_commerce/model/Address.java
@@ -0,0 +1,39 @@
+package com.uade.tpo.e_commerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "addresses")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+
+    private String city;
+
+    private String province;
+
+    private String postalCode;
+
+    @OneToOne(mappedBy = "address")
+    @JsonIgnore
+    private User user;
+}
