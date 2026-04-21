addb4f8a74bff9a15d5423ba91727c9563eab077
Nahuel Berensztein <nberensztein13@gmail.com>
2026-04-21 20:35:01 -0300

    entidad pedidos

---
+++ src/main/java/com/uade/tpo/e_commerce/model/Order.java
@@ -0,0 +1,54 @@
+package com.uade.tpo.e_commerce.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder.Default;
+
+@Data
+@Entity
+@Table(name="orders")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "order_products",
+        joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id", referencedColumnName = "id")
+    )
+    @Default
+    private List<Product> products = new ArrayList<>();
+
+    private Double total;
+
+}
