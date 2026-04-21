7f9c2c8b9557e1bbcfc4b03554dc4ec63e02a7dc
Nahuel Berensztein <nberensztein13@gmail.com>
2026-04-21 20:36:47 -0300

    DTOs de dirección

---
+++ src/main/java/com/uade/tpo/e_commerce/model/dto/AddressCreateDTO.java
@@ -0,0 +1,20 @@
+package com.uade.tpo.e_commerce.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AddressCreateDTO {
+    
+    private String street;
+
+    private String city;
+
+    private String province;
+
+    private String postalCode;
+
+}
