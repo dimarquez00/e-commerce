e93070e2a3a251c0863cca18a75bacc2e554ce89
laraluciiano <lara.kentros@gmail.com>
2026-04-22 19:58:48 -0300

    configuracion de seguridad

---
+++ src/main/java/com/uade/tpo/e_commerce/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.uade.tpo.e_commerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.uade.tpo.e_commerce.repository.UserRepository;
+import com.uade.tpo.e_commerce.security.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    
+    private final JwtFilter jwtFilter; // filtro que valida el JWT
+    private final UserRepository userRepository; // acceso a usuarios en la base de datos
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //busca usuario por mail para autenticacion
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean //authenticationmanager de spring security
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean //encripta contraseñas con el bcrypt
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) //desactiva CSRF para APIs REST
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll() //permite acceso libre a auth
+                .anyRequest().authenticated()) //el resto requiere autenticación
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); //agrega el filtro JWT
+
+        return http.build();
+    }
+}
