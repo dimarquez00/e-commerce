4f0bb8a2b2ce3c18a4455273f4cfd929c044486b
dimarquez00 <dimarquez@uade.edu.ar>
2026-06-18 19:47:45 -0300

    Fix: corrige import de store en main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { ContextProvider } from './context/ContextProvider.jsx';
-
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Provider store={store}>
-            <ContextProvider>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </ContextProvider>
-        </Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StrictMode>,
 )
